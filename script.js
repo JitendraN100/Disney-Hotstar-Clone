c87bfe2d37176c747e5c8fdb2a9044bc373472ab
JitendraN100 <jitendranahak275@gmail.com>
2023-08-03 17:22:32 +0530

    corrections

--- script.js
+++ script.js
@@ -3,30 +3,30 @@ let movies =
     {
         name: "falcon and the winter soldier",
         des: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe exercitationem quam perspiciatis explicabo nesciunt eos accusantium inventore illum expedita mollitia!",
-        image: "images/slider 2.png"
+        image: "Images/slider 2.png"
     },
 
     {
         name: "loki",
         des: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe exercitationem quam perspiciatis explicabo nesciunt eos accusantium inventore illum expedita mollitia!",
-        image: "images/slider 1.png"
+        image: "Images/slider 1.png"
     },    
     
     {
         name: "wanda vision",
         des: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe exercitationem quam perspiciatis explicabo nesciunt eos accusantium inventore illum expedita mollitia!",
-        image: "images/slider 3.png"
+        image: "Images/slider 3.png"
     },
 
     {
         name: "raya and the last dragon",
         des: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe exercitationem quam perspiciatis explicabo nesciunt eos accusantium inventore illum expedita mollitia!",
-        image: "images/slider 4.png"
+        image: "Images/slider 4.png"
     },
     {
         name: "luca",
         des: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe exercitationem quam perspiciatis explicabo nesciunt eos accusantium inventore illum expedita mollitia!",
-        image: "images/slider 5.png"
+        image: "Images/slider 5.png"
     }
 ];
 
